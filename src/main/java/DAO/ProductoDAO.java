a54837c5313d6d27805ae88b378f8010a11f5e1b
abril troncoso <abrilpotitroncoso@gmail.com>
2025-09-08 17:35:41 -0300

    implementacion de producto DAO

--- src/main/java/DAO/ProductoDAO.java
+++ src/main/java/DAO/ProductoDAO.java
@@ -1,5 +1,15 @@
+package DAO;
+
 import entity.Producto;
+import java.util.List;
 
 public interface ProductoDAO    {
-
+    public void insertar(Producto producto);
+    public void actualizar(Producto producto);
+    public void eliminar(Producto producto);
+    public List<Producto> obtenerTodos();
+    public Producto obtenerPorId(long id);
+    public List<Producto> obtenerPorNombre(String nombre);
+    public List<Producto> obtenerPorCategoria(String categoria);
+    public List<Producto> obtenerPorPrecio(float precio);
 }
