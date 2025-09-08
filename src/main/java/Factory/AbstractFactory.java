13d21fdcd9b42b785a7335e4bec6696dcec3dc25
Agostina <agoslaffitte17@gmail.com>
2025-09-08 16:30:28 -0300

    abstract factory creado

---
+++ src/main/java/Factory/AbstractFactory.java
@@ -0,0 +1,17 @@
+public abstract class AbstractFactory{
+    public static final int MYSQL_JDBC=1;
+    public abstract ProductoDAO getProductoDAO();
+    public abstract ClienteDAO getClienteDAO();
+    public abstract FacturaDAO getFacturaDAO();
+    public abstract FacturaProductoDAO getFacturaProductoDAO();
+    public static AbstractFactory getDAOFactory(int whichFactory){
+        switch(whichFactory){
+            case 'MYSQL_JDBC':{
+                return MySQLDAOFactory.getInstance();
+
+            }
+            default: return null;
+        }
+    }
+
+}
