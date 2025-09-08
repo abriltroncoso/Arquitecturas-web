bd0ff1511287d2ef97458f9f6254e2632a0fcf20
ValentinBulnes <bulnesvalentin@gmail.com>
2025-09-08 16:23:53 -0300

    entidades completas

--- src/main/java/entity/FacturaProducto.java
+++ src/main/java/entity/FacturaProducto.java
@@ -1,4 +1,46 @@
 package entity;
 
 public class FacturaProducto {
+    private Integer idFactura;
+    private Integer idProducto;
+    private Integer cantidad;
+
+    public FacturaProducto(Integer idFactura, Integer idProducto, Integer cantidad) {
+        this.idFactura = idFactura;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+    }
+
+    public Integer getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "FacturaProducto{" +
+                "idFactura=" + idFactura +
+                ", idProducto=" + idProducto +
+                ", cantidad=" + cantidad +
+                '}';
+    }
 }
